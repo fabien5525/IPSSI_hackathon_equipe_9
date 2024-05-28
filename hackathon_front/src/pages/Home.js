1ba5443b3e700b76c0e4df55fbe15ff20ee37851
Omar Mufti <pro.omarmufti@gmail.com>
2024-05-28 16:23:20 +0200

    update 2

--- hackathon_front/src/pages/Home.js
+++ hackathon_front/src/pages/Home.js
@@ -1,42 +1,42 @@
 import React from 'react';
 import { Container, Box, Typography, Grid, Avatar } from '@mui/material';
 import GitHubIcon from '@mui/icons-material/GitHub';
-import logo from '../assets/OlympicsAI.png';  // Assurez-vous que le logo est dans ce chemin
+import logo from '../assets/olympic.svg';  // Assurez-vous que le logo est dans ce chemin
 
 const teamMembers = [
-  { name: 'Aastha Arora', image: 'path/to/aastha.png' }, // Remplacez par le chemin réel de l'image
-  { name: 'Dianne Jardinez', image: 'path/to/dianne.png' }, // Remplacez par le chemin réel de l'image
-  { name: 'Duong Luu', image: 'path/to/duong.png' }, // Remplacez par le chemin réel de l'image
-  { name: 'Ritika Bhansali', image: 'path/to/ritika.png' }, // Remplacez par le chemin réel de l'image
-  { name: 'Swarna Guntaka', image: 'path/to/swarna.png' }, // Remplacez par le chemin réel de l'image
-  { name: 'Votre Nom', image: 'path/to/votre_image.png' } // Remplacez par votre image
+  { name: 'Fabien COLARD', image: 'path/to/aastha.png' }, // Remplacez par le chemin réel de l'image
+  { name: 'Loris HASLAY', image: 'path/to/dianne.png' }, // Remplacez par le chemin réel de l'image
+  { name: 'Chaymae HOUBBADI', image: 'path/to/duong.png' }, // Remplacez par le chemin réel de l'image
+  { name: 'Jemimadoria KOUMBA', image: 'path/to/ritika.png' }, // Remplacez par le chemin réel de l'image
+  { name: 'Omar MUFTI', image: 'path/to/swarna.png' }, // Remplacez par le chemin réel de l'image
+  { name: 'Mamoudou NDONGO', image: 'path/to/votre_image.png' } // Remplacez par votre image
 ];
 
 function Home() {
   return (
     <Container maxWidth="lg">
       <Box textAlign="center" my={4}>
-        <img src={logo} alt="Olympics Logo" width="200" />
+        <img src={logo} alt="Olympics Logo" width="400" />
         <Typography variant="h3" component="h1" gutterBottom>
-          RACE TO THE FINISH LINE
+          COURSE VERS LA LIGNE D'ARRIVÉE
         </Typography>
       </Box>
 
       <Box my={4}>
         <Typography variant="h4" component="h2" color="primary" gutterBottom>
-          PROJECT OBJECTIVES
+          OBJECTIFS DU PROJET
         </Typography>
         <Typography variant="body1" gutterBottom>
-          1. Analyze and create visualizations on an Olympic dataset from 1896 to 2016 to uncover which countries stay at the top overtime, by season, and by sport; what it takes for an Olympian to be at the top for each sport, sport popularity, and the relationship between Olympic Medal counts and country's GDP.
+          Analyser et créer des visualisations sur un ensemble de données olympiques de 1896 à 2016 pour découvrir quels pays restent en tête au fil du temps, par saison et par sport ; ce qu'il faut pour qu'un Olympien soit au sommet dans chaque sport, la popularité des sports et la relation entre le nombre de médailles olympiques et le PIB du pays.
         </Typography>
         <Typography variant="body1" gutterBottom>
-          2. Additionally from the Olympic dataset, machine learning was incorporated to predict the amount of medals 25 countries would obtain in the 2020 Tokyo Olympics with Linear Regression, Logistic Regression, and Auto Regressive Integrated Moving Average (ARIMA) model.
+          En outre, à partir de l'ensemble de données olympiques, l'apprentissage automatique a été intégré pour prédire le nombre de médailles que 25 pays obtiendraient aux Jeux olympiques de Tokyo 2020 avec la régression linéaire, la régression logistique et le modèle ARIMA (Auto Regressive Integrated Moving Average).
         </Typography>
       </Box>
 
       <Box my={4}>
         <Typography variant="h4" component="h2" color="primary" gutterBottom>
-          TEAM
+          ÉQUIPE
         </Typography>
         <Grid container spacing={4} justifyContent="center">
           {teamMembers.map((member) => (
@@ -55,26 +55,26 @@ function Home() {
 
       <Box my={4}>
         <Typography variant="h4" component="h2" color="primary" gutterBottom>
-          KEY TAKEAWAYS
+          POINTS CLÉS
         </Typography>
         <Box>
           <Typography variant="h6" component="h3">
-            WHAT WENT WELL
+            CE QUI A BIEN FONCTIONNÉ
           </Typography>
           <Typography variant="body1" gutterBottom>
-            • Creating the following charts: Racing barchart, static bar and line chart, and Leaflet map with two different layers (street map and choropleth) with Plotly.js, D3.js, Chart.js, and Leaflet.js.
+            • Création des graphiques suivants : diagramme à barres dynamique, diagramme à barres statique et graphique linéaire, ainsi qu'une carte Leaflet avec deux couches différentes (carte des rues et carte choroplèthe) avec Plotly.js, D3.js, Chart.js et Leaflet.js.
           </Typography>
         </Box>
         <Box>
           <Typography variant="h6" component="h3">
-            CHALLENGES WE OVERCAME
+            DÉFIS QUE NOUS AVONS SURMONTÉS
           </Typography>
           <Typography variant="body1" gutterBottom>
-            • Utilizing and connecting Python Flask-powered RESTful API.
+            • Utilisation et connexion d'une API RESTful alimentée par Python Flask.
             <br />
-            • Web browser cache accurately displaying visualizations.
+            • Cache du navigateur web affichant correctement les visualisations.
             <br />
-            • New Machine Learning model: ARIMA.
+            • Nouveau modèle d'apprentissage automatique : ARIMA.
           </Typography>
         </Box>
       </Box>
